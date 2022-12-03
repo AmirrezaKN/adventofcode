256332170908c350e7d9d367a07f510f843ebc78
Amirreza Kafshnochi <amirrezanochi@gmail.com>
2022-12-04 01:53:26 +0330

    solved day3 part 2

--- day03/03.go
+++ day03/03.go
@@ -28,3 +28,39 @@ func Question02_1(input []string) int {
 
 	return total
 }
+
+func Question02_2(input []string) int {
+	total := 0
+
+	for i := 0; i < len(input); i += 3 {
+		if i+2 > len(input) {
+			break
+		}
+
+		seenMap := make(map[rune][]bool)
+		for _, r := range input[i] {
+			if len(seenMap[r]) == 0 {
+				seenMap[r] = append(seenMap[r], true)
+			}
+		}
+
+		for _, r := range input[i+1] {
+			if len(seenMap[r]) == 1 {
+				seenMap[r] = append(seenMap[r], true)
+			}
+		}
+
+		for _, r := range input[i+2] {
+			if len(seenMap[r]) == 2 {
+				if unicode.IsLower(r) {
+					total += int(r) - 96
+				} else {
+					total += int(r) - 38
+				}
+				break
+			}
+		}
+	}
+
+	return total
+}
--- day03/03_test.go
+++ day03/03_test.go
@@ -3,6 +3,8 @@ package day03
 import (
 	"app/helper"
 	"testing"
+
+	"gotest.tools/assert"
 )
 
 func TestQuestion02_1(t *testing.T) {
@@ -29,6 +31,39 @@ func TestQuestion02_1(t *testing.T) {
 			if got := Question02_1(tt.args.input); got != tt.want {
 				t.Errorf("Question02_1() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestQuestion02_2(t *testing.T) {
+	lines := helper.GetInputLines("./03_input.txt")
+
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "0",
+			args: args{
+				input: []string{"vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg", "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"},
+			},
+			want: 70,
+		},
+		{
+			name: "1",
+			args: args{
+				input: lines,
+			},
+			want: 7889,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, Question02_2(tt.args.input))
 		})
 	}
 }
